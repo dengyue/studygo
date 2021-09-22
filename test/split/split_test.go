04e08a2673389e2f36c35440d3c7eb38aeb172e0
dengyue <zdy@aliyun.com>
2021-09-22 20:54:23 +0800

    test

--- test/split/split_test.go
+++ test/split/split_test.go
@@ -20,3 +20,21 @@ func Test2Split(t *testing.T) {
 		t.Fatalf("want:%v but got: %v\n", want, ret)
 	}
 }
+
+func Test3Split(t *testing.T) {
+	type testCase struct {
+		input string
+		sep   string
+		want  []string
+	}
+	testGroups := []testCase{
+		{"abcdabcd", "a", []string{"", "bcd", "bcd"}},
+		{"abc,,,,abc", ",", []string{"abc", "", "", "", "abc"}},
+	}
+	for _, tg := range testGroups {
+		ret := Split(tg.input, tg.sep)
+		if !reflect.DeepEqual(ret, tg.want) {
+			t.Fatalf("want:%v but got: %v\n", tg.want, ret)
+		}
+	}
+}
